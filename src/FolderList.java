2c3a7cd3038204d79ebe1b07d1ebaa5dec1a7813
MASK <mask@pisem.net>
2015-09-17 08:01:32 +0300

    Just begin

---
+++ src/FolderList.java
@@ -0,0 +1,10 @@
+/**
+ * Created by MASK on 17/09/2015.
+ */
+public class FolderList {
+
+    String path = "M:\\TestFolder\\";
+
+    public FolderList(String path) {
+    }
+}
---
+++ src/Run.java
@@ -0,0 +1,9 @@
+/**
+ * Created by MASK on 17/09/2015.
+ */
+public class Run {
+
+    public static void main(String[] args) {
+
+    }
+}
