42b7823db52e5eeda0099436b0b188d33c5e063d
EvaSokol <mask@pisem.net>
2015-09-22 12:07:30 +0300

    To split file name

--- src/FolderList.java
+++ src/FolderList.java
@@ -1,10 +1,23 @@
+import java.util.ArrayList;
+import java.io.File;
+
 /**
  * Created by MASK on 17/09/2015.
  */
 public class FolderList {
 
-    String path = "M:\\TestFolder\\";
+//    String path = "C:\\TestFolder\\";
+    ArrayList <String> FileList;
 
-    public FolderList(String path) {
+    public void getFileList(String path) {
+        File folder = new File(path);
+        File[] listOfFiles = folder.listFiles();
+        for (File cur_file : listOfFiles) {
+            System.out.println(cur_file.getName());
+            System.out.println(cur_file.getName().split(".").length);
+//            System.out.println(cur_file.getName().split(".")[1]);
+        }
     }
+
+
 }
--- src/Run.java
+++ src/Run.java
@@ -1,9 +1,12 @@
 /**
  * Created by MASK on 17/09/2015.
  */
+
 public class Run {
 
     public static void main(String[] args) {
+        FolderList ff = new FolderList();
+        ff.getFileList("C:\\TestFolder\\");
 
     }
 }
